17b13ad8f93056be32fa631576865324eac131e3
Ziyu Liu <zl3220@columbia.edu>
2022-10-05 21:39:49 +0000

    part 3 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,11 +2,11 @@ class Movie < ActiveRecord::Base
   def self.all_ratings
     ['G','PG','PG-13','R']
   end
-  def self.with_ratings(col,ratings_list)
+  def self.with_ratings(column,ratings_list)
     # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
     #  movies with those ratings
     # if ratings_list is nil, retrieve ALL movies
-      if not col 
+      if not column 
         if (not ratings_list) or ratings_list.empty?
           return Movie.all
         else
@@ -14,9 +14,9 @@ class Movie < ActiveRecord::Base
         end
       else 
         if (not ratings_list) or ratings_list.empty?
-          return Movie.all.order(col)
+          return Movie.all.order(column)
         else
-          return Movie.where(rating: ratings_list).order(col)
+          return Movie.where(rating: ratings_list).order(column)
         end
       end     
   end  
